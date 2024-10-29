ffd7faae5be986662b55d82d406014522f5fb1fd
Antanu Dutta <antanu.bittu@gmail.com>
2024-10-29 12:53:52 +0530

    understanding react components

--- react-antanu/src/App.jsx
+++ react-antanu/src/App.jsx
@@ -1,3 +1,24 @@
 export const App = () => {
-  return <h1>Hello Antanu Dutta</h1>;
+  return (
+    <div>
+      <NetflixSeries />
+    </div>
+  );
+};
+
+const NetflixSeries = () => {
+  return (
+    <div>
+      <div>
+        <img src="1.jpg" alt="" width={"40%"} height={"40%"} />
+      </div>
+      <h2>Name: Queen Of Tears</h2>
+      <h3>Rating 8.2</h3>
+      <p>
+        Sumamry:Baek Hyun-woo and Hong Hae-in navigate a tense relationship,
+        both at home and at work. But upon deciding his future, Hyun-woo pays a
+        visit to his family.
+      </p>
+    </div>
+  );
 };
