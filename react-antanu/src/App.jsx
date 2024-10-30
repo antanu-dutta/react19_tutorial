b673c4683bc8dccf06fb22e8dfd071b2523176f4
Antanu Dutta <antanu.bittu@gmail.com>
2024-10-30 10:28:38 +0530

    conditional rendering

--- react-antanu/src/App.jsx
+++ react-antanu/src/App.jsx
@@ -13,11 +13,28 @@ const NetflixSeries = () => {
   const summary = `Baek Hyun-woo and Hong Hae-in navigate a tense relationship,
         both at home and at work. But upon deciding his future, Hyun-woo pays a
         visit to his family.`;
+  let age = 16;
 
   const returnGenre = () => {
-    const genre = 'Romcom'
-    return genre
-  }
+    const genre = "Romcom";
+    return genre;
+  };
+
+  // if (age < 18) {
+  //   return (
+  //     <div>
+  //       <div>
+  //         <img src="1.jpg" alt="" width={"40%"} height={"40%"} />
+  //       </div>
+  //       <h2>Name: {name}</h2>
+  //       <h3>Rating {rating}</h3>
+  //       <p>Sumamry:{summary}</p>
+  //       <p>Total Count : {2 + 8 + 2}</p>
+  //       <p>Genre: {returnGenre()}</p>
+  //       <button>Not Available</button>
+  //     </div>
+  //   );
+  // }
 
   return (
     <div>
@@ -27,8 +44,9 @@ const NetflixSeries = () => {
       <h2>Name: {name}</h2>
       <h3>Rating {rating}</h3>
       <p>Sumamry:{summary}</p>
-      <p>Total Count : {2+8+2}</p>
+      <p>Total Count : {2 + 8 + 2}</p>
       <p>Genre: {returnGenre()}</p>
+      <button>{age >= 18 ? "Watch Now" : "Not available"}</button>
     </div>
   );
 };
