6462b94bc8a37122473c3ddcce375e6263f2bff3
Antanu Dutta <antanu.bittu@gmail.com>
2024-10-30 10:16:09 +0530

    dynamic value in jsx

--- react-antanu/src/App.jsx
+++ react-antanu/src/App.jsx
@@ -2,23 +2,33 @@ export const App = () => {
   return (
     <div>
       <NetflixSeries />
+      <NetflixSeries />
     </div>
   );
 };
 
 const NetflixSeries = () => {
+  const name = "Queen Of Tears";
+  const rating = "8.2";
+  const summary = `Baek Hyun-woo and Hong Hae-in navigate a tense relationship,
+        both at home and at work. But upon deciding his future, Hyun-woo pays a
+        visit to his family.`;
+
+  const returnGenre = () => {
+    const genre = 'Romcom'
+    return genre
+  }
+
   return (
     <div>
       <div>
         <img src="1.jpg" alt="" width={"40%"} height={"40%"} />
       </div>
-      <h2>Name: Queen Of Tears</h2>
-      <h3>Rating 8.2</h3>
-      <p>
-        Sumamry:Baek Hyun-woo and Hong Hae-in navigate a tense relationship,
-        both at home and at work. But upon deciding his future, Hyun-woo pays a
-        visit to his family.
-      </p>
+      <h2>Name: {name}</h2>
+      <h3>Rating {rating}</h3>
+      <p>Sumamry:{summary}</p>
+      <p>Total Count : {2+8+2}</p>
+      <p>Genre: {returnGenre()}</p>
     </div>
   );
 };
