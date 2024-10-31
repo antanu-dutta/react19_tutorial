36bfed3296df27684a0d385ae04c4edd8c6f969d
Antanu Dutta <antanu.bittu@gmail.com>
2024-10-31 18:26:05 +0530

    tailwind css included

--- react-antanu/src/components/SeriesCard.jsx
+++ react-antanu/src/components/SeriesCard.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 /* eslint-disable react/prop-types */
 const SeriesCard = ({ curElem }) => {
-  const { img_url, name, rating, description, watch_url } = curElem; // ? Props
+  const { img_url, name, rating, description, watch_url, genre } = curElem; // ? Props
 
   const Button = styled.button`
     padding: 10px 12px;
@@ -21,7 +21,7 @@ const SeriesCard = ({ curElem }) => {
       <div>
         <img src={img_url} alt={name} style={{ width: "100%" }} />
       </div>
-      <h2>Name: {name}</h2>
+      <h2 className="text-red-900 my-4 text-3xl">Name: {name}</h2>
       <h3>
         Rating{" "}
         <span
@@ -32,13 +32,17 @@ const SeriesCard = ({ curElem }) => {
           {rating}
         </span>
       </h3>
-      <p style={{ margin: "1.2rem" }}>
+      <p style={{ marginBlock: "1.2rem" }}>
         Sumamry:{description.slice(0, 15) + "..."}
       </p>
-      <p>Genre:</p>
+      <p>
+        Genre:
+        {genre.map((curElem, index) => (
+          <span key={index}> {curElem}, </span>
+        ))}
+      </p>
       <a href={watch_url} target="_blank">
         <Button rating={rating}>Watch Now</Button>
-        {/* <button style={myStyle}>Watch Now</button> */}
       </a>
     </li>
   );
