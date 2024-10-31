aeee5a3f219eefbb2510d58fdf57837b139c5062
Antanu Dutta <antanu.bittu@gmail.com>
2024-10-31 13:48:56 +0530

    inline css style

--- react-antanu/src/components/SeriesCard.jsx
+++ react-antanu/src/components/SeriesCard.jsx
@@ -1,14 +1,17 @@
 /* eslint-disable react/prop-types */
 const SeriesCard = ({ curElem }) => {
   const { img_url, name, rating, description, watch_url } = curElem;
+
   return (
-    <li>
+    <li className="card">
       <div>
-        <img src={img_url} alt={name} style={{ width: "300px" }} />
+        <img src={img_url} alt={name} style={{ width: "100%" }} />
       </div>
       <h2>Name: {name}</h2>
       <h3>Rating {rating}</h3>
-      <p>Sumamry:{description}</p>
+      <p style={{ margin: "1.2rem" }}>
+        Sumamry:{description.slice(0, 15) + "..."}
+      </p>
       <p>Genre:</p>
       <a href={watch_url} target="_blank">
         Watch Now
