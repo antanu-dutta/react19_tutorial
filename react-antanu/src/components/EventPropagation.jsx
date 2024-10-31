264ee26c5a523e5999e2ef05d9e64958d6cb14d3
Antanu Dutta <antanu.bittu@gmail.com>
2024-10-31 22:54:09 +0530

    Event Propagation in React

---
+++ react-antanu/src/components/EventPropagation.jsx
@@ -0,0 +1,7 @@
+export const EventPropagation = () => {
+  return (
+    <div>
+      <h1>hellow world</h1>
+    </div>
+  );
+};
--- react-antanu/src/main.jsx
+++ react-antanu/src/main.jsx
@@ -3,14 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 // import { App } from "./App.jsx";
 // import { EventHandling } from "./components/EventHandling.jsx";
-import { EventProps } from "./components/EventProps.jsx";
+// import { EventProps } from "./components/EventProps.jsx";
+import { EventPropagation } from "./components/EventPropagation.jsx";
 // import { Practices } from "./Practices.jsx";
 // import Profile from "./components/Profile.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <EventHandling /> */}
-    <EventProps />
+    {/* <EventProps /> */}
+    <EventPropagation />
     {/* <App /> */}
     {/* <Practices /> */}
     {/* <Profile /> */}
