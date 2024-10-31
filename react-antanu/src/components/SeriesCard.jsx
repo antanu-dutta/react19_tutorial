6f9b62930368c008e7e85bae6743b5cccb2465aa
Antanu Dutta <antanu.bittu@gmail.com>
2024-10-31 13:58:37 +0530

    conditional styling

--- react-antanu/src/components/SeriesCard.jsx
+++ react-antanu/src/components/SeriesCard.jsx
@@ -2,19 +2,29 @@
 const SeriesCard = ({ curElem }) => {
   const { img_url, name, rating, description, watch_url } = curElem;
 
+  const myStyle = {
+    padding: "10px 12px",
+    marginTop: "10px",
+  };
+
   return (
     <li className="card">
       <div>
         <img src={img_url} alt={name} style={{ width: "100%" }} />
       </div>
       <h2>Name: {name}</h2>
-      <h3>Rating {rating}</h3>
+      <h3>
+        Rating{" "}
+        <span className={`rating ${rating >= 8.5 ? "super_hit" : "average"}`}>
+          {rating}
+        </span>
+      </h3>
       <p style={{ margin: "1.2rem" }}>
         Sumamry:{description.slice(0, 15) + "..."}
       </p>
       <p>Genre:</p>
       <a href={watch_url} target="_blank">
-        Watch Now
+        <button style={myStyle}>Watch Now</button>
       </a>
     </li>
   );
