1d8b49a617ed6bfc98211d0601ff2f62e21ceba6
Antanu Dutta <antanu.bittu@gmail.com>
2024-11-02 13:26:30 +0530

    Login Form Created

--- react-antanu/src/main.jsx
+++ react-antanu/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Resgistration from "./components/hooks/Resgistration";
+// import Resgistration from "./components/hooks/Resgistration";
+import LoginForm from "./components/hooks/LoginForm";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Resgistration />
+    <LoginForm />
   </StrictMode>
 );
